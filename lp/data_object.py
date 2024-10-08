e36cfe0df6a739694dd0197ac639b35775b29aed
Egor Sazhin <sazhin2003.egor@gmail.com>
2024-10-08 22:44:43 +0500

    master - Добавлены фильтры

--- lp/data_object.py
+++ lp/data_object.py
@@ -1,6 +1,12 @@
 class DataObject:
-    def __init__(self, c, a, b, A):
+    def __init__(self, c, a, b, A, j, Y):
         self.c = c
         self.a = a
         self.b = b
         self.A = A
+        self.j = j
+        self.x = None
+        self.Y = Y
+
+    def set_x(self, value):
+        self.x = value
--- lp/math.py
+++ lp/math.py
@@ -1,51 +1,60 @@
 from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpStatus, value
+
 from data_object import DataObject
-from input_data import c, a, b, A, Q, P
+from input_data import c, a, b, A, j, Y
 
 # Создание объекта данных
-data_list = [DataObject(c_j, a_j, b_j, A_jc) for c_j, a_j, b_j, A_jc in zip(c, a, b, A)]
+data_list = [DataObject(c_j, a_j, b_j, A_jc, j_i, y_j) for c_j, a_j, b_j, A_jc, j_i, y_j in zip(c, a, b, A, j, Y)]
 
-data_filter = []
-for data in data_list:
-    if (8 <= data.A <= 9):
-        data_filter += data
+# Погрешность
+infelicity = 0.5
+data_filter_list = [data for data in data_list if (8 - infelicity <= data.A <= 9 + infelicity)
+                    and ((data.Y is not None) and (17 - infelicity <= data.Y <= 19 + infelicity))
+                    ]
 
-# Создание задачи минимизации
-problem = LpProblem("Minimize_Coke_Cost", LpMinimize)
 
-# Создание переменных (x_j) для каждой марки угля
-x = [LpVariable(f"x{i+1}", lowBound=0) for i in range(28)]
+def find_solution():
+    # Создание задачи минимизации
+    problem = LpProblem("Minimize_Coke_Cost", LpMinimize)
 
-# Целевая функция
-problem += lpSum(c[i] * x[i] for i in range(28))
+    # Создание переменных (x_j) для каждой марки угля и добавление их в data_filter
+    for data in data_filter_list:
+        data.set_x(LpVariable(f"x{data.j}", lowBound=0))
 
-# Ограничение на сумму долей
-problem += lpSum(x) == 1
+    # Целевая функция
+    problem += lpSum(data.c * data.x for data in data_filter_list)
 
-# Ограничения на минимальное и максимальное участие
-for i, (a_i, b_i) in enumerate(zip(a, b)):
-    if a_i is not None:
-        problem += x[i] <= a_i
-        print(a_i)
-    if b_i is not None:
-        problem += x[i] >= b_i
-        print(b_i)
+    x = [x.x for x in data_filter_list]
+    # Ограничение на сумму долей
+    problem += lpSum(x) == 1
 
-# # Ограничение на капитальные вложения
-# problem += lpSum(q[i] * x[i] for i in range(len(a))) <= Q / P
+    # Ограничения на минимальное и максимальное участие
+    for data in data_filter_list:
+        if data.a is not None:
+            problem += data.x <= data.a
+            print(data.a)
+        if data.b is not None:
+            problem += data.x >= data.b
+            print(data.b)
 
-# Решение задачи
-problem.solve()
+    # Решение задачи
+    problem.solve()
 
-# Вывод результатов
-print(f"Статус: {LpStatus[problem.status]}")
-print("============================")
-print(f"Оптимальная стоимость: {value(problem.objective)}")
-print("Оптимальные переменные:")
-test_x_sum = 0
-for v in problem.variables():
-    if v.varValue != 0:
-        test_x_sum += v.varValue
-        print(f"{v.name} = {v.varValue}")
-print("============================")
-print("sum(x) =", test_x_sum)
+    # Вывод результатов
+    print(f"Статус: {LpStatus[problem.status]}")
+    print("============================")
+    print(f"Оптимальная стоимость: {value(problem.objective)}")
+    print("Оптимальные переменные:")
+    test_x_sum = 0
+    for v in problem.variables():
+        if v.varValue != 0:
+            test_x_sum += v.varValue
+            print(f"{v.name} = {v.varValue}")
+    print("============================")
+    print("sum(x) =", test_x_sum)
+
+
+if not data_filter_list:
+    print("Решений нет")
+else:
+    find_solution()
