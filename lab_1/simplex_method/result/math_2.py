ecc8e01a23747c72798f26436219b2eef5b185d2
Egor Sazhin <sazhin2003.egor@gmail.com>
2024-10-29 20:39:51 +0500

    master - Перенес код в lab_1

--- lab_1/simplex_method/result/math_2.py
+++ lab_1/simplex_method/result/math_2.py
@@ -1,7 +1,7 @@
 from scipy.optimize import linprog
 
-from lp.data_object import DataObject
-from lp.input_data import c, a, b, A, j, Y, brand
+from lab_1.lp.data_object import DataObject
+from lab_1.lp.input_data import c, a, b, A, j, Y, brand
 
 # Создание объекта данных
 data_list = [DataObject(c_j, a_j, b_j, A_jc, j_i, y_j, brand_j) for c_j, a_j, b_j, A_jc, j_i, y_j, brand_j in zip(c, a, b, A, j, Y, brand)]
@@ -29,34 +29,6 @@ def find_solution(data_list):
             A_ub.append([0] * i + [-1] + [0] * (len(data_list) - i - 1))
             b_ub.append(-data.b)
 
-    # # Бинарные переменные для выбора варианта
-    # V_1 = 0
-    # V_2 = 0
-    # V_3 = 0
-
-    # # Ограничения на выбор одного варианта
-    # A_eq.append([0] * len(data_list))
-    # b_eq.append(1)
-
-    # # Ограничения на марки угля для каждого варианта
-    # k_j_vars = [i for i, data in enumerate(data_list) if data.brand in [K, J]]
-    # g_oc_ss_vars = [i for i, data in enumerate(data_list) if data.brand in [G, OC, CC]]
-    #
-    # A_eq.append([1 if i in k_j_vars else 0 for i in range(len(data_list))])
-    # b_eq.append(0.5 * V_1 + 0.2 * V_2 + 0.1 * V_3)
-    #
-    # A_eq.append([1 if i in g_oc_ss_vars else 0 for i in range(len(data_list))])
-    # b_eq.append(0.5 * V_1 + 0.8 * V_2 + 0.9 * V_3)
-
-    # # Ограничения для случаев, когда нет решений
-    # A_eq.append([1 if data.brand == K else 0 for data in data_list])
-    # b_eq.append(0.5 * V_1)
-    #
-    # A_eq.append([1 if data.brand == J else 0 for data in data_list])
-    # b_eq.append(0.5 * V_1)
-    #
-    # A_eq.append([1 if data.brand in [OC, CC] else 0 for data in data_list])
-    # b_eq.append(0.5 * V_1)
 
     # Решение задачи
     result = linprog(c_coeffs, A_eq=A_eq, b_eq=b_eq, A_ub=A_ub, b_ub=b_ub, method='simplex')
